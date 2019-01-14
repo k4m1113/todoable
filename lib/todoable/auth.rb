a083d6cbdf690bb38512a2733ae8c3a1b87c0caa
Kamille Delgardo <kamilledelgardo@gmail.com>
2019-01-13 22:47:37 -0800

    basic auth

--- lib/todoable/auth.rb
+++ lib/todoable/auth.rb
@@ -1,6 +1,24 @@
+require 'net/http'
+require 'time'
+
 class Todoable::Auth
   def initialize
-    
+    uri = URI('http://todoable.teachable.tech/api/authenticate')
+    file = File.stat 'authenticate'
+    req = Net::HTTP::Post.new(uri)
+    req.basic_auth 'kamilledelgardo@gmail.com', 'todoable'
+    req['If-Modified-Since'] = file.mtime.rfc2822
+    req['Content-Type'] = "application/json"
+    req['Accept'] = "application/json"
+
+    res = Net::HTTP.start(uri.hostname, uri.port) {|http|
+      http.request(req)
+    }
+
+    open 'authenticate', 'w' do |io|
+      io.write res.body
+    end if res.is_a?(Net::HTTPSuccess)
+
   end
 
   def refresh
